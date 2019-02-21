05156f3660b2595ea4219b0a9ddb79b29fbd31bb
Andrew Trinh <Andrew Trinh>
2019-02-20 20:48:10 -0700

    commit -am does didn't add these files

---
+++ pi/src/modules/mqtt_communications.py
@@ -0,0 +1,41 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+class CommunicationHandler():
+
+    def __init__(self, ENDPOINT, CA, CERT, PRIVATE_KEY):
+        try:
+            print("test")
+            self.mqtt_client = AWSIoTMQTTClient("client")
+            self.mqtt_client.configureEndpoint(ENDPOINT, 8883)
+            self.mqtt_client.configureCredentials(CA, PRIVATE_KEY, CERT)
+            self.mqtt_client.configureOfflinePublishQueueing(-1)
+            self.mqtt_client.configureDrainingFrequency(2)
+            self.mqtt_client.configureConnectDisconnectTimeout(10)
+            self.mqtt_client.configureMQTTOperationTimeout(5)
+            self.initilized = True
+        except FailedToCreateMQTTConnection:
+            print("Error: MqttClient Construction Failed")
+            self.initilized = False
+
+    def send_payload(self, data):
+        if data in("", '', None):
+            print("Error: Message Empty")
+            return False
+        payload = '{ID: "Success", Data: '+str(data)+' }'
+        try:
+            self.mqtt_client.connect()
+            self.mqtt_client.publish("testConnection", payload, 0)
+            print("Publish success.")
+            return True
+        except FailedToSendError:
+            print("Publish failed.")
+            return False
+
+class Error(Exception):
+    """Base class for other exceptions"""
+
+class FailedToSendError(Error):
+    """The device has failed to send the payload to AWS"""
+
+class FailedToCreateMQTTConnection(Error):
+    """The device has failed connect to AWS"""

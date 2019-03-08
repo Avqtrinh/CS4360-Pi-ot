7d6d0c1cf804bbc402915edc790a642a294b0138
Andrew Trinh <Andrew Trinh>
2019-03-07 17:33:55 -0700

    redoing test for controller

--- pi/src/controller.py
+++ pi/src/controller.py
@@ -3,19 +3,27 @@ import sys
 from time import sleep
 from modules.Motion import PirSensor
 from modules.mqtt_communications import CommunicationHandler
-sys.path.insert(0, 'modules/*')
 
-ENDPOINT = "a2vjr670r30pov-ats.iot.us-east-2.amazonaws.com"
-CA = "certinfo/CA.pem"
-CERT = "certinfo/29c44ddf59-certificate.pem.crt"
-PRIVATE_KEY = 'certinfo/29c44ddf59-private.pem.key'
-COM_HANDLER = CommunicationHandler(ENDPOINT, CA, CERT, PRIVATE_KEY)
-#create settings ini later for this
-PIR = PirSensor(4)
-while True:
-    PAYLOAD = None
-    PAYLOAD = PIR.look_for_motion()
-    if PAYLOAD is not None:
-        COM_HANDLER.send_payload(PAYLOAD)
-        print(PAYLOAD)
-        sleep(5)
+class Controller():
+    def __init__(self):
+        self.endpoint = "a2vjr670r30pov-ats.iot.us-east-2.amazonaws.com"
+        self.ca = sys.path[0]+"/certinfo/CA.pem"
+        self.cert = sys.path[0]+"/certinfo/29c44ddf59-certificate.pem.crt"
+        self.private_key = sys.path[0]+'/certinfo/29c44ddf59-private.pem.key'
+        self.com_handler = CommunicationHandler(self.endpoint, self.ca, self.cert, self.private_key)
+        self.pir = PirSensor(4)
+        self.payload = ""
+    def run(self):
+        try:
+            while True:
+                self.payload = self.pir.look_for_motion()
+                if self.payload is not None:
+                    self.com_handler.send_payload(self.payload)
+                    print(self.payload)
+                    sleep(5)
+        except:
+            print("message failed to send")
+
+if __name__ == '__main__':
+    controller = Controller()
+    controller.run()
--- pi/src/tests/controller_test.py
+++ pi/src/tests/controller_test.py
@@ -1,7 +1,11 @@
 import sys
+from time import sleep
 import unittest
 from unittest.mock import MagicMock
-from time import sleep
+from unittest.mock import patch
+from gpiozero.pins.mock import MockFactory, MockPin, MockConnectedPin
+from gpiozero import Device
+from gpiozero import exc
 path_length = sys.path[0].index("tests")
 sys.path.insert(0,sys.path[0][0:path_length])
 from modules.Motion import PirSensor
@@ -10,13 +14,19 @@ import controller
 class TestPiController(unittest.TestCase):
 
     def test__init__(self):
+        Device.pin_factory = MockFactory()
         test_controller = Controller()
-        self.assertIsNotNone(test_controller.COM_HANDLER)
-        self.assertNotEqual(test_controller.PAYLOAD , 'TEST')
-        test_controller.PIR.look_for_motion = MagicMock(return_value = 'TEST')
-        self.assertEqual(test_controller.PAYLOAD , 'TEST')
-        #self.assertFalse(test_controller.COM_HANDLER.send_payload(PAYLOAD))
-        #self.assertTrue(test_controller.COM_HANDLER.send_payload(PAYLOAD))
+        self.assertIsNotNone(test_controller.com_handler)
+        self.assertIsNotNone(test_controller.pir)
+        test_controller.com_handler = MagicMock()
+        test_controller.pir = MagicMock()
+        test_controller.com_handler.send_payload = MagicMock(return_value = 1)
+        test_controller.pir.look_for_motion = MagicMock(return_value = "TestMessage")
+        test_controller.run()
+        self.assertEqual(test_controller.payload,"TestMessage")
+        test_controller.pir.look_for_motion = MagicMock(return_value = "")
+        test_controller.run()
+        self.assertEqual(test_controller.payload,"")
 
 if __name__ == '__main__':
     unittest.main()

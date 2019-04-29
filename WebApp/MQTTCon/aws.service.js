92b6ae37339bba7a6c293c1ca55bf1396498cd2c
Andrew Trinh <Andrew Trinh>
2019-04-28 18:57:21 -0600

    finishing up we lost the progress somewhere

--- WebApp/MQTTCon/aws.service.js
+++ WebApp/MQTTCon/aws.service.js
@@ -13,19 +13,18 @@ var logS3 = new AWS.S3({
 });
 app.use(cors())
 app.set('json spaces',0)
-
 app.get('/api_logPing',(req,res)=>{
   var params = {
     Bucket:"pidatalog",
     MaxKeys: 10
   }
-  logS3.listObjectsV2(params, function(err,data){ //error here somewhere
+  logS3.listObjectsV2(params, function(err,data){
     if (err) console.log(err,err.stack);
     else buildSendListOfObjects(data,res);
   });
 });
+var listItems = []
 function buildSendListOfObjects(data,res){
-  var listItems = []
   data['Contents'].forEach(function(item,index){
     var params = {
       Bucket:"pidatalog",
@@ -33,11 +32,11 @@ function buildSendListOfObjects(data,res){
     }
     logS3.getObject(params, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
-        else    temp = JSON.parse(data["Body"].toString()), temp["Key"] = item["Key"], listItems.push(temp);           // successful response
+        else   temp = JSON.parse(data["Body"].toString()), temp["Key"] = item["Key"], listItems.push((item['key'],temp));           // successful response
     });
-    console.log(listItems)
   });
-  //res.send(listItems)
+  res.send(listItems);
+  listItems = [];
 }
 app.listen(3001, ()=>{
   console.log('server started');

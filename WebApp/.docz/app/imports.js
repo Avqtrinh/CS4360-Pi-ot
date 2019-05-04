ca77931d9423850c97cdc16407395afbe8407a07
KEV-PC\Kevin <kngovand@gmail.com>
2019-05-04 12:43:03 -0600

    all .mdx set up, go crazy!

--- WebApp/.docz/app/imports.js
+++ WebApp/.docz/app/imports.js
@@ -17,4 +17,48 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "react-app-react-app" */ 'react-app/react-app.mdx'
     ),
+  'react-app/src/index.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-index" */ 'react-app/src/index.mdx'
+    ),
+  'react-app/src/modules/App.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-app" */ 'react-app/src/modules/App.mdx'
+    ),
+  'react-app/src/modules/Dashboard.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-dashboard" */ 'react-app/src/modules/Dashboard.mdx'
+    ),
+  'react-app/src/modules/Home.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-home" */ 'react-app/src/modules/Home.mdx'
+    ),
+  'react-app/src/modules/Login.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-login" */ 'react-app/src/modules/Login.mdx'
+    ),
+  'react-app/src/modules/Log.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-log" */ 'react-app/src/modules/Log.mdx'
+    ),
+  'react-app/src/modules/Logout.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-logout" */ 'react-app/src/modules/Logout.mdx'
+    ),
+  'react-app/src/modules/Navigation.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-navigation" */ 'react-app/src/modules/Navigation.mdx'
+    ),
+  'react-app/src/modules/Routes.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-routes" */ 'react-app/src/modules/Routes.mdx'
+    ),
+  'react-app/src/modules/Signup.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-signup" */ 'react-app/src/modules/Signup.mdx'
+    ),
+  'react-app/src/modules/Stats.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "react-app-src-modules-stats" */ 'react-app/src/modules/Stats.mdx'
+    ),
 }
